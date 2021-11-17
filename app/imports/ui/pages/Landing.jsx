d0fe9c3a61206f08f7c565a6cf1a8c7d6668a55b
Breadonn <braydonn@hawaii.edu>
2021-11-16 23:25:58 -1000

    Initial Landing Page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,38 +1,59 @@
 import React from 'react';
-import { Container, Label, Icon, Header, Image } from 'semantic-ui-react';
+import { Container, Label, Icon, Header, Image, Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
       <div>
-        <Container className='messageBox'>
-          <div className='landingTop'>
-            <Header as='h1' textAlign='center' inverted>Welcome to OHA Catbot!</Header>
-            <Label color='blue' size='huge'>
-              <Image src='https://img1.picmix.com/output/stamp/normal/1/1/6/7/1917611_5dcbf.png' size='small'/>
-            Catbot
+        <div className='landingTop'>
+          <Header as='h1' textAlign='center' inverted className='heading'>Welcome to the OHA Catbot landing page! Click on one of the buttons to explore the website</Header>
+          <div className='block'>
+            <Label as='a' color='blue' size='massive' className='pageLabel'>
+              <img src='https://img1.picmix.com/output/stamp/normal/1/1/6/7/1917611_5dcbf.png' alt='catjam'/> Catbot
+            </Label>
+            <Label as='a' color='blue' size='massive' className='pageLabel2'>
+              <Icon name='book'/> Tutorial
+            </Label>
+            <Label as='a' color='blue' size='massive' className='pageLabel3'>
+              <Icon name='reply'/> Send Feedback
             </Label>
           </div>
+        </div>
 
+        <div className='landingBottom'>
           <Container>
-            <Label color='olive' size='big'>
-              <Icon name='phone'/>
+            <Header as='h2' textAlign='center'>Contacts us or visit our website!</Header>
+            <Grid columns={3}>
+              <Grid.Column textAlign='center'>
+                <Image src='https://icones.pro/wp-content/uploads/2021/05/icone-iphone-noire.png' size='small' centered/>
+                <Label color='olive' size='big' centered>
+                  <Icon name='phone'/>
           Phone
-              <Label.Detail><a href="tel: 808-594-1835" className="white-text" >808-594-1835</a></Label.Detail>
-            </Label>
-            <Label color='orange' size='big'>
-              <Icon name='mail'/>
+                  <Label.Detail><a href="tel: 808-594-1835" className="white-text" >808-594-1835</a></Label.Detail>
+                </Label>
+              </Grid.Column>
+
+              <Grid.Column textAlign='center'>
+                <Image src='https://www.shareicon.net/data/512x512/2016/07/14/606965_gmail_4096x4096.png' size='small' centered/>
+                <Label color='orange' size='big'>
+                  <Icon name='mail'/>
           Email
-              <Label.Detail><a href="mailto: info@oha.org" className="white-text">info@oha.org</a></Label.Detail>
-            </Label>
-            <Label color='teal' size='big'>
-              <Icon name='mouse pointer'/>
+                  <Label.Detail><a href="mailto: info@oha.org" className="white-text">info@oha.org</a></Label.Detail>
+                </Label>
+              </Grid.Column>
+
+              <Grid.Column textAlign='center'>
+                <Image src='https://static.thenounproject.com/png/3080133-200.png' size='small' centered/>
+                <Label color='teal' size='big'>
+                  <Icon name='mouse pointer'/>
           Website
-              <Label.Detail><a target="_blank" rel="noopener noreferrer" href="https://www.oha.org/" className="white-text">https://www.oha.org/</a></Label.Detail>
-            </Label>
+                  <Label.Detail><a target="_blank" rel="noopener noreferrer" href="https://www.oha.org/" className="white-text">https://www.oha.org/</a></Label.Detail>
+                </Label>
+              </Grid.Column>
+            </Grid>
           </Container>
-        </Container>
+        </div>
       </div>
     );
   }
